bf01948af0196bf6e4f4a66e9b379c29d5266dad
Samitha99 <73548027+Samitha99@users.noreply.github.com>
2022-09-24 21:49:30 +0530

    next js , pklock.js, index.js from config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,4 +5,12 @@ module.exports = {
     loader: "akamai",
     path: "",
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.optimization.splitChunks.cacheGroups = { }
+    config.optimization.minimize = true;
+    return config
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+  }
 };
